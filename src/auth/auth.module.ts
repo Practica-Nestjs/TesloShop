d91df7e35fea0e52dbec4f09e060a48b766fed84
Hernan Velasquez <velasquezmhernan@gmail.com>
2023-01-22 01:50:52 -0500

    feat: :sparkles: Metodo de validacion
    Agregando un Provider que permita usar la estrategia para validad del Jwt, permitiendo informar si el token es valido/el usuario esa activo.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 
 import { User } from './entities/user.entity';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
+    ConfigModule,
     TypeOrmModule.forFeature([User]),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.registerAsync({
@@ -24,13 +26,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         };
       },
     }),
-    // JwtModule.register({
-    //   secret: process.env.JWT_SECRET,
-    //   signOptions: { expiresIn: '2h' },
-    // }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
-  exports: [TypeOrmModule],
+  providers: [AuthService, JwtStrategy],
+  exports: [TypeOrmModule, JwtStrategy, PassportModule, JwtModule],
 })
 export class AuthModule {}
