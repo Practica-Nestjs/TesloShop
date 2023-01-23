28d123bd9bfe134ccf9ead5a8c56fa075f27238f
Hernan Velasquez <velasquezmhernan@gmail.com>
2023-01-23 01:17:09 -0500

    feat: :sparkles: Mejorando decorador de Usuario y Generando Raw-Headers decorator
    Generando decoradores customisados, para extraer inormacion de la data, de manera mas facil, adeas de mejorar los decoradores existentes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,8 @@
-import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
+import { Controller, Post, Body, Get, UseGuards, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 import { AuthService } from './auth.service';
-import { GetUser } from './decorators/get-user.decorator';
+import { GetUser, RawHeaders } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 
@@ -22,8 +22,20 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute(@GetUser() user: User) {
-    console.log({ user });
-    return { ok: true, message: 'Hello Word Private', user };
+  testingPrivateRoute(
+    @Req() request: Express.Request,
+    @GetUser() user: User,
+    @GetUser('email') emailUser: string,
+    @RawHeaders() rawHeaders: string[],
+  ) {
+    console.log({ rawHeaders });
+
+    return {
+      ok: true,
+      message: 'Hello Word Private',
+      user,
+      emailUser,
+      rawHeaders,
+    };
   }
 }
