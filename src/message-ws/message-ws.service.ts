446a997220e565308314eae9ef5480a511061b5e
Hernan Velasquez <velasquezmhernan@gmail.com>
2023-01-28 21:48:59 -0500

    feat: :sparkles: Agregando seguridad
    Conectando comunicacion en el modulo de conexcion, permitiendo tener mas seguridad y evitando que  se repitan las multiples conexiones al servidor y desconectandolas anteriores

--- src/message-ws/message-ws.service.ts
+++ src/message-ws/message-ws.service.ts
@@ -1,23 +1,52 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Socket } from 'socket.io';
+import { Repository } from 'typeorm';
+import { User } from '../auth/entities/user.entity';
 
 interface ConnectedClients {
-  [id: string]: Socket;
+  [id: string]: {
+    socket: Socket;
+    user: User;
+  };
 }
 
 @Injectable()
 export class MessageWsService {
   private connectedClients: ConnectedClients = {};
 
-  registerClient(client: Socket) {
-    this.connectedClients[client.id] = client;
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async registerClient(client: Socket, idUser: string) {
+    const user = await this.userRepository.findOneBy({ id: idUser });
+    if (!user) throw new Error(`User Not Found`);
+    if (!user.isActive) throw new Error(`User not Active`);
+    this.checkUserConnection(user);
+    this.connectedClients[client.id] = { socket: client, user: user };
   }
 
   removeClient(clientId: string) {
     delete this.connectedClients[clientId];
   }
 
-  getConnectedClients(): number {
-    return Object.keys(this.connectedClients).length;
+  getConnectedClients(): string[] {
+    return Object.keys(this.connectedClients);
+  }
+
+  getUserFullNameBySoketId(soketId: string) {
+    return this.connectedClients[soketId].user.fullname;
+  }
+
+  private checkUserConnection(user: User) {
+    for (const clientId of Object.keys(this.connectedClients)) {
+      const connectedClient = this.connectedClients[clientId];
+
+      if (connectedClient.user.id === user.id) {
+        connectedClient.socket.disconnect();
+        break;
+      }
+    }
   }
 }
